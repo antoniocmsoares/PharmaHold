ab6f9b8288dfe5ead114001deec163d39ecb4f9d
PIMENTA\marti <martimpimentaribeiro007@gmail.com>
2025-11-12 12:19:41 +0000

    ADD PRODUCTS CLASS(NEED FIX)

---
+++ PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/Products.java
@@ -0,0 +1,11 @@
+package pt.ipp.estg.pp.pharmahold;
+
+public class Products {
+    private int productId;
+    private boolean productState;
+    private String productName;
+    private float productPrice;
+
+    private int currentStock;
+    private int minStock;
+}
