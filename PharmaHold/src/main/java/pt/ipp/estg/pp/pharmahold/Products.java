3905f468fcdfa5038240e55cadde34a700cecab8
PIMENTA\marti <martimpimentaribeiro007@gmail.com>
2025-11-12 16:09:34 +0000

    MELHOREI O PRODUTOS (VER)

--- PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/Products.java
+++ PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/Products.java
@@ -1,5 +1,5 @@
 package pt.ipp.estg.pp.pharmahold;
-
+import java.util.ArrayList;
 public class Products {
     private int productId;
     private boolean productState;
@@ -8,4 +8,17 @@ public class Products {
 
     private int currentStock;
     private int minStock;
+
+    private static ArrayList<Products> productsList = new ArrayList<>();
+
+    public Products(int productId, boolean productState, String productName, float productPrice, int minStock) {
+        this.productId = productId;
+        this.productState = productState;
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    public static void addProducts(Products produto) {
+        productsList.add(produto);
+    }
 }
