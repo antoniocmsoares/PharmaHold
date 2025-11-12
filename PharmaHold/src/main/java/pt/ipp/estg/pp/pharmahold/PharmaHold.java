64feb76637dcf1fefdfef42a25c66890b6b9cdd6
DESKTOP-89LKOBU\anton <antoniocmsoares23@gmail.com>
2025-11-12 11:24:17 +0000

    0.0.2

--- PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/PharmaHold.java
+++ PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/PharmaHold.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package pt.ipp.estg.pp.pharmahold;
 
-/**
- *
- * @author anton
- */
 public class PharmaHold {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Prescription pres = new Prescription();
     }
 }
---
+++ PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/Prescription.java
@@ -0,0 +1,9 @@
+package pt.ipp.estg.pp.pharmahold;
+
+public class Prescription {
+
+    public Prescription() {
+        String name = "22FO3";
+        System.out.println(name);
+    }
+}
