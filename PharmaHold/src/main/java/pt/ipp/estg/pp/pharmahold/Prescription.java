04d023031087b7c758b9aad40b53d2e8a9b0ffed
DESKTOP-89LKOBU\anton <antoniocmsoares23@gmail.com>
2025-11-18 16:48:56 +0000

    fuck u

--- PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/Prescription.java
+++ PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/Prescription.java
@@ -1,10 +1,13 @@
 package pt.ipp.estg.pp.pharmahold;
 
-import pt.ipp.estg.pp.pharmahold.ENUMS.*;
 import java.util.ArrayList;
+
+import pt.ipp.estg.pp.pharmahold.ENUMS.DoctorNames;
+import pt.ipp.estg.pp.pharmahold.ENUMS.PrescriptionType;
 
 public class Prescription {
     private int id = 0;
+    private int idUser;
     private int[] emiDate = new int[3]; //emition date :D dd/mm/yy
     private int[] expDate = new int[3]; //expire date :| dd/mm/yy
     private PrescriptionType type; //tipo da receita, se é receita comum ou controlada...
@@ -19,12 +22,13 @@ public class Prescription {
         this.type = type;
         this.doctor = name;
         this.prescriptionState = true;
+        existentPrescriptions.add(this);
     }
-    
+    /* fucked for now
     public static void newPrescription(int[] emiDate, int[] expDate,PrescriptionType preType, DoctorNames preDoctor) {
         Prescription presc = new Prescription(emiDate, expDate, preType, preDoctor);
         existentPrescriptions.add(presc);
-    }
+    }*/
 
     public static Prescription getPrescriptionById(int requestedId) {
         for (int i = 0; i < existentPrescriptions.size(); i++) {
@@ -37,6 +41,6 @@ public class Prescription {
     }
 
     public String toString() {
-        return "** Requested Prescription **\n|_ id: " + id + "\n|_ type: " + type + "\n|_ doctor: " + doctor;
+        return "** Requested Prescription **\n|_ id: " + id + "\n|_ type: " + type + "\n|_ doctor: " + doctor + "\n|_ emission date: " + emiDate[0] + "/" + emiDate[1] + "/" +emiDate[2] + "\n|_ expiration date: " + expDate[0] + "/" + expDate[1] + "/" +expDate[2];
     }
 }
