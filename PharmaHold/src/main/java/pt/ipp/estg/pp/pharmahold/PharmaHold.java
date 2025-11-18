ae166cf6470043fcc626ea6b1eae9202e3914ab6
DESKTOP-89LKOBU\anton <antoniocmsoares23@gmail.com>
2025-11-18 18:24:50 +0000

    id getter prescription

--- PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/PharmaHold.java
+++ PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/PharmaHold.java
@@ -4,8 +4,8 @@ import pt.ipp.estg.pp.pharmahold.ENUMS.DoctorNames;
 import pt.ipp.estg.pp.pharmahold.ENUMS.PrescriptionType;
 
 public class PharmaHold {
-    public static Prescription pres1 = new Prescription(new int[] {01,01,2026},new int[] {01,01,2026},PrescriptionType.COMMON, DoctorNames.ANA_SANTOS);
-    public static Prescription pres2 = new Prescription(new int[] {01,01,2026},new int[] {01,01,2026},PrescriptionType.COMMON, DoctorNames.ARTUR_SOUSA);
+    public static Prescription pres1 = new Prescription(new int[] {01,01,2026},new int[] {02,01,2026},PrescriptionType.COMMON, DoctorNames.ANA_SANTOS);
+    public static Prescription pres2 = new Prescription(new int[] {01,01,2026},new int[] {02,01,2026},PrescriptionType.COMMON, DoctorNames.ARTUR_SOUSA);
 
     public static void main(String[] args) {
         System.out.println("=== PROGRAM DEBUG OUTPUT ==="); //só para orientação
@@ -14,12 +14,12 @@ public class PharmaHold {
         System.out.println(pres2);
 
         // make a precription request
-        int requestedId = 2;
+        int requestedId = 0;
         Prescription test = Prescription.getPrescriptionById(requestedId);
         if (test != null) {
-            System.out.println("_suc: Prescription founded!");
+            System.out.println("_suc: Prescription added!" + pres1.getId());
             test.toString();
         }
-        else System.out.println("_err: The object Prescription with the id " + requestedId + " could'nt be found.");
+        else System.out.println("_err: The object Prescription with the id " + requestedId + " couldn't be found.");
     }
 }
--- PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/Prescription.java
+++ PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/Prescription.java
@@ -6,29 +6,25 @@ import pt.ipp.estg.pp.pharmahold.ENUMS.DoctorNames;
 import pt.ipp.estg.pp.pharmahold.ENUMS.PrescriptionType;
 
 public class Prescription {
-    private int id = 1;
-    private int idUser;
+    private int id;
     private int[] emiDate = new int[3]; //emition date :D dd/mm/yy
     private int[] expDate = new int[3]; //expire date :| dd/mm/yy
     private PrescriptionType type; //tipo da receita, se é receita comum ou controlada...
     private DoctorNames doctor;
     private boolean prescriptionState;
+
     static private ArrayList<Prescription> existentPrescriptions = new ArrayList<>(); //todas as existentes
+    private static int countID = 1;
 
     public Prescription(int[] emiDate, int[] expDate,PrescriptionType type, DoctorNames name) {
-        this.id = id++;
+        this.id = countID++;
         this.emiDate = emiDate;
-        this.emiDate = expDate;
+        this.expDate = expDate;
         this.type = type;
         this.doctor = name;
         this.prescriptionState = true;
         existentPrescriptions.add(this);
     }
-    /* fucked for now (not required, may be usefull)
-    public static void newPrescription(int[] emiDate, int[] expDate,PrescriptionType preType, DoctorNames preDoctor) {
-        Prescription presc = new Prescription(emiDate, expDate, preType, preDoctor);
-        existentPrescriptions.add(presc);
-    }*/
 
     public static Prescription getPrescriptionById(int requestedId) {
         for (int i = 0; i < existentPrescriptions.size(); i++) {
@@ -40,7 +36,11 @@ public class Prescription {
         return null;
     }
 
+    public int getId(){
+        return this.id;
+    }
+
     public String toString() {
-        return "** Requested Prescription **\n|_ id: " + id + "\n|_ type: " + type + "\n|_ doctor: " + doctor + "\n|_ emission date: " + emiDate[0] + "/" + emiDate[1] + "/" +emiDate[2] + "\n|_ expiration date: " + expDate[0] + "/" + expDate[1] + "/" +expDate[2];
+       return "** Requested Prescription **\n|_ id: " + id + "\n|_ type: " + type + "\n|_ doctor: " + doctor + "\n|_ emission date: " + emiDate[0] + "/" + emiDate[1] + "/" +emiDate[2] + "\n|_ expiration date: " + expDate[0] + "/" + expDate[1] + "/" +expDate[2];
     }
 }
