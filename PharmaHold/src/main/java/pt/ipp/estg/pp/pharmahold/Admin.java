aa0b7f63bea6647f2ccb89f79966ed8b3f031b4f
PIMENTA\marti <martimpimentaribeiro007@gmail.com>
2025-11-19 12:32:29 +0000

    NEW UPDATE TO ADMIN

--- PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/Admin.java
+++ PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/Admin.java
@@ -2,4 +2,17 @@ package pt.ipp.estg.pp.pharmahold;
 
 public class Admin {
 
+
+    public void getName() {
+        
+
+    }
+    public boolean login(int type, String name, String password) {
+        if (type == 1) {
+            return false;
+        } 
+        else {
+            return true;
+        }
+    }
 }
