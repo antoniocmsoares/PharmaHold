c07c2c74bcaac65ff6bd203079cd2dc04462e3ec
PIMENTA\marti <martimpimentaribeiro007@gmail.com>
2025-11-19 09:40:35 +0000

    no message

--- PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/Users.java
+++ PharmaHold/src/main/java/pt/ipp/estg/pp/pharmahold/Users.java
@@ -1,15 +1,16 @@
 package pt.ipp.estg.pp.pharmahold;
 
-import java.util.ArrayList;
-
-import pt.ipp.estg.pp.pharmahold.ENUMS.UserTypes;
-
 public class Users {
+    private static int countId = 1;
     private int id;
-    private UserTypes type;
     private String name;
-    private String passwowrd;
+    private String password;
     private int contact; // format  000 000 000
-    private ArrayList<Orders> orders;
-    private int totalPoints;
+
+    public Users(String name, String password, int contact) {
+        this.id = countId++;
+        this.name = name;
+        this.password = password;
+        this.contact = contact;
+    }
 }
